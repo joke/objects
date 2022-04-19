d5f014de473d2531d778036d9e3f47e63f99d9c1
Joke de Buhr <joke@xckk.de>
2022-04-19 19:00:52 +0200

    refactor: split tests
    Signed-off-by: Joke de Buhr <joke@xckk.de>

---
+++ tests/src/test/groovy/io/github/joke/objects/tests/BeanFieldTest.groovy
@@ -0,0 +1,34 @@
+package io.github.joke.objects.tests
+
+import io.github.joke.objects.tests.basic.PersonImpl
+import spock.lang.Specification
+
+import static org.junit.platform.commons.util.ReflectionUtils.HierarchyTraversalMode.TOP_DOWN
+import static org.junit.platform.commons.util.ReflectionUtils.findFields
+import static org.junit.platform.commons.util.ReflectionUtils.isNotFinal
+import static org.junit.platform.commons.util.ReflectionUtils.isNotStatic
+import static org.junit.platform.commons.util.ReflectionUtils.isPrivate
+
+class BeanFieldTest extends Specification {
+
+    def 'field attributes'() {
+        setup:
+        def field = findFields(PersonImpl, {it.name == 'name'}, TOP_DOWN).first()
+
+        expect:
+        isPrivate field
+        isNotFinal field
+        isNotStatic field
+    }
+
+    def 'field with interface type'() {
+        setup:
+        def field = findFields(PersonImpl, {it.name == 'parents'}, TOP_DOWN).first()
+
+        expect:
+        isPrivate field
+        isNotFinal field
+        isNotStatic field
+    }
+
+}

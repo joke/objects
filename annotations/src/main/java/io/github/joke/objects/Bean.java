4d5c365e13125b4ea8cbf4a4a3175c2d2322c80b
Joke de Buhr <joke@xckk.de>
2022-04-22 18:16:07 +0200

    fix: correct annotation retention
    Signed-off-by: Joke de Buhr <joke@xckk.de>

--- annotations/src/main/java/io/github/joke/objects/Bean.java
+++ annotations/src/main/java/io/github/joke/objects/Bean.java
@@ -5,10 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 @Documented
 @Target(TYPE)
-@Retention(RUNTIME)
+@Retention(SOURCE)
 public @interface Bean {
 }

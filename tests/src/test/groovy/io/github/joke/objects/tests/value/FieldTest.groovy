bd00465906983eebfab9b36c5c3c24f358b3bf4b
Joke de Buhr <joke@xckk.de>
2022-04-23 15:16:23 +0200

    feat: added Value tests
    Signed-off-by: Joke de Buhr <joke@xckk.de>

---
+++ tests/src/test/groovy/io/github/joke/objects/tests/value/FieldTest.groovy
@@ -0,0 +1,35 @@
+package io.github.joke.objects.tests.value
+
+
+import io.github.joke.objects.tests.bean.PersonImpl
+import spock.lang.Specification
+
+import static org.junit.platform.commons.util.ReflectionUtils.HierarchyTraversalMode.TOP_DOWN
+import static org.junit.platform.commons.util.ReflectionUtils.findFields
+import static org.junit.platform.commons.util.ReflectionUtils.isNotFinal
+import static org.junit.platform.commons.util.ReflectionUtils.isNotStatic
+import static org.junit.platform.commons.util.ReflectionUtils.isPrivate
+
+class FieldTest extends Specification {
+
+    def 'field attributes'() {
+        setup:
+        def field = findFields(PersonImpl, {it.name == 'name'}, TOP_DOWN).first()
+
+        expect:
+        isPrivate field
+        isNotFinal field
+        isNotStatic field
+    }
+
+    def 'field with interface type'() {
+        setup:
+        def field = findFields(PersonImpl, {it.name == 'parents'}, TOP_DOWN).first()
+
+        expect:
+        isPrivate field
+        isNotFinal field
+        isNotStatic field
+    }
+
+}
